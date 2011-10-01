14e18dde7ef79025252fb91c979fa2731f010b30
Colin Merkel <colin.merkel@gmail.com>
2011-09-30 22:20:28 -0400

    added login form

---
+++ copy/copy.php
@@ -0,0 +1,10 @@
+<?php
+    
+define("HEADER_MENU1","about 2025");
+define("HEADER_MENU2","latest");
+define("HEADER_MENU3","bios");
+define("HEADER_MENU4","contact");
+   
+define("HEADER_LOGIN_HERE","Login here, if you want.");
+	
+?>
--- index.php
+++ index.php
@@ -1,41 +1,32 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
-   "http://www.w3.org/TR/html4/strict.dtd">
+<?php include("header.php"); ?>
 
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title>twenty twenty-five</title>
-	<meta name="author" content="Colin" />
-	<!-- Date: 2011-09-29 -->
 	
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-
-	<script src="js/bg_full.js"></script>
-				
-	<link rel=stylesheet href="style/style.css" />
+<script type=text/javascript>
 	
-	<script type=text/javascript>
-		var menuitem = 0;
-		
-		function golink(target) {
-			if(target == menuitem) return;
-			if(target == 0) {
-				$('#content_shim').hide('fast');
-				$('#content').hide('fast');
-				menuitem = 0;
-				return;
-			}
-			$('#content_shim').hide('fast');
-			$('#content').hide('fast',function () {
-					
-					$('#content').html($('.menuitem'+target).html());
-					$('#content').show('fast');
-					$('#content_shim').show('fast');				
-			});
-			menuitem = target;			
-		}
-		
-	</script>
+var menuitem = 0;
+	
+function golink(target) {
+	if(target == menuitem) return;
+	if(target == 0) {
+		$('#login_box').show('fast');
+		$('#login_shim').show('fast');
+		$('#content_shim').hide('fast');
+		$('#content').hide('fast');
+		menuitem = 0;
+		return;
+	}
+	$('#login_box').hide('fast');
+	$('#login_shim').hide('fast');
+	$('#content_shim').hide('fast');
+	$('#content').hide('fast',function () {
+			
+			$('#content').html($('.menuitem'+target).html());
+			$('#content').show('fast');
+			$('#content_shim').show('fast');				
+	});
+	menuitem = target;			
+}
+</script>
 	
 	
 </head>
@@ -47,49 +38,33 @@
 	<img  onClick="golink(0)" src="/images/2025.png" />
 	
 	<ul> 
-		<li onClick="golink(1)">about 2025</li>
-		<li onClick="golink(2)">latest</li>
-		<li onClick="golink(3)">bios</li>
-		<li onClick="golink(4)">contact</li>	
+		<li onClick="golink(1)"><?php echo HEADER_MENU1;?> </li>
+		<li onClick="golink(2)"><?php echo HEADER_MENU2;?></li>
+		<li onClick="golink(3)"><?php echo HEADER_MENU3;?></li>
+		<li onClick="golink(4)"><?php echo HEADER_MENU4;?></li>	
 	</ul>
 	
 </div>
 
-<div id=content>
-		
-</div> 
-
-<div class='menuitem1 hiding'>
-
-	<h1>Hi.</h1>
-	<p>Welcome to 2025 laboratories.</p>
-	<p>We are an engineering collective interested in software, electronics, robotics, and the future. </p>
-	
+<div id=login_box>
+		<span><?php echo HEADER_LOGIN_HERE; ?></span>
+		<form id=login_form action='login.php' method='post'>
+			<input autofocus type=text placeholder="username" name=username />
+			<input type=password placeholder="password" name=password />
+			<input type="submit" value="Submit the form!" style="position: absolute; top: 0; left: 0; z-index: 0; width: 1px; height: 1px; visibility: hidden;" />
+		</form>
 </div>
+<div id="login_shim"> </div>
 
-<div class='menuitem2 hiding'>
-	<h1>A crazy app we made</h1>
-	<p>IT does some crazy things</p>
-	<p>Man let me tell you it is so crazy yo</p>
-	
-</div>
+<div id=content></div> 
 
-<div class='menuitem3 hiding'>
-	<h1>Employee A</h1>
-	<p>I never liked him very much</p>
-	<h1>Employee B</h1>
-	<p>I never liked him very much either</p>
-		
-</div>
-
-<div class='menuitem4 hiding'>
-	<h1>Phone us</h1>
-	<p>We have a phone</p>
-	<p>etc</p>
-</div>
-
+<div class='menuitem1 hiding'><?php echo INDEX_ABOUT; ?></div>
+<div class='menuitem2 hiding'><?php echo INDEX_LATEST; ?></div>
+<div class='menuitem3 hiding'><?php echo INDEX_BIOS; ?></div>
+<div class='menuitem4 hiding'><?php echo INDEX_CONTACT; ?></div>
 
 <div id="shim"> </div>
 <div id="content_shim"> </div>
-</body>
-</html>
+
+<?php include("footer.php"); ?>
+
